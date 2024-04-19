8d85eae743968ef4c475e53f2dfc609386242e92
Guilherme <guilhermeshadow91@gmail.com>
2024-04-19 19:14:39 -0300

    delete: deleting useless files

--- src/kafka/transaction.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TransactionProducerService } from './transaction-producer.service';
-
-@Module({
-  providers: [TransactionProducerService],
-  exports: [TransactionProducerService],
-})
-export class TransactionModule {}
